12a6bb6e867d82c399ca8d546e7f4e864dcafe6a
Marcin Restel <marcinrestel@gmail.com>
2018-03-17 22:51:05 +0100

    update modal

---
+++ src/app/components/customModal/customModal.js
@@ -0,0 +1,32 @@
+angular
+  .module("app")
+  .component('customModal', {
+    templateUrl: 'app/components/customModal/customModal.html',
+    bindings: {
+      item: '=',
+      active: '='
+    },
+    controller: customModalController
+
+  });
+
+  function customModalController(){
+    var vm = this;
+    vm.changeSlide = changeSlide;
+
+    vm.$onInit = function(){
+      vm.activeSlide = 0;
+    }
+
+    function changeSlide(slideNumber, length){
+      if(slideNumber < length && slideNumber >= 0){
+        vm.activeSlide = slideNumber;
+      }
+      else if (slideNumber >= length){
+        vm.activeSlide = length - 1;
+      }
+      else if (slideNumber < 0){
+        vm.activeSlide = 0;
+      }
+    }
+  }
--- src/app/main.js
+++ src/app/main.js
@@ -7,18 +7,18 @@ angular
 
   function appCtrl($scope, $http, MODAL_SCROLL_CONFIG, DATABASE) {
     var vm = this;
-    vm.changeModalShowcaseImage = changeModalShowcaseImage;
+    // vm.changeModalShowcaseImage = changeModalShowcaseImage;
     vm.jsModalImagesScrollConfig = MODAL_SCROLL_CONFIG;
-    vm.showPortfolioItem = showPortfolioItem;
+    vm.getPortfolioItemData = getPortfolioItemData;
 
     init();
 
-    function changeModalShowcaseImage(event) {
-        vm.modalShowcaseImage = vm.modalImages[event.currentTarget.attributes['data-id'].value].LQ;
-        angular.element(".js-modal-image.active").removeClass("active");
-        angular.element(".js-modal-image[data-id = " + event.currentTarget.attributes['data-id'].value + "]").addClass("active");
-        setModalShowcaseImagePrevAndNext(event.currentTarget.attributes['data-id'].value);
-    }
+    // function changeModalShowcaseImage(event) {
+    //     vm.modalShowcaseImage = vm.modalImages[event.currentTarget.attributes['data-id'].value].LQ;
+    //     angular.element(".js-modal-image.active").removeClass("active");
+    //     angular.element(".js-modal-image[data-id = " + event.currentTarget.attributes['data-id'].value + "]").addClass("active");
+    //     setModalShowcaseImagePrevAndNext(event.currentTarget.attributes['data-id'].value);
+    // }
 
     function init() {
         var response = DATABASE;
@@ -61,35 +61,39 @@ angular
 
     }
 
-    function setModalShowcaseImagePrevAndNext(currentID) {
-        let current = parseInt(currentID);
-        let prev = current - 1;
-        let next = current + 1;
-        let max = vm.modalImages.length - 1;
+    // function setModalShowcaseImagePrevAndNext(currentID) {
+    //     let current = parseInt(currentID);
+    //     let prev = current - 1;
+    //     let next = current + 1;
+    //     let max = vm.modalImages.length - 1;
 
-        vm.prevModalShowcaseImageID = prev < 0 ? 0 : prev;
-        vm.nextModalShowcaseImageID = next >= max ? max : next;
-    }
+    //     vm.prevModalShowcaseImageID = prev < 0 ? 0 : prev;
+    //     vm.nextModalShowcaseImageID = next >= max ? max : next;
+    // }
 
-    function showPortfolioItem(event) {
-        let clickedItemID = event.currentTarget.attributes['data-id'].value;
-        angular.element(event.currentTarget).addClass('active');
+    // function showPortfolioItem(event) {
+    //     let clickedItemID = event.currentTarget.attributes['data-id'].value;
+    //     angular.element(event.currentTarget).addClass('active');
 
-        vm.modalTitle = vm.portfolioItems[clickedItemID].name;
-        vm.modalDescription = vm.portfolioItems[clickedItemID].description;
-        vm.modalImages = vm.portfolioItems[clickedItemID].imagesList;
-        vm.modalShowcaseImage = vm.modalImages[0].LQ;
-        setModalShowcaseImagePrevAndNext(0);
+    //     vm.modalTitle = vm.portfolioItems[clickedItemID].name;
+    //     vm.modalDescription = vm.portfolioItems[clickedItemID].description;
+    //     vm.modalImages = vm.portfolioItems[clickedItemID].imagesList;
+    //     vm.modalShowcaseImage = vm.modalImages[0].LQ;
+    //     setModalShowcaseImagePrevAndNext(0);
 
-        angular.element('#portfolio-modal').modal('show', {
-            keyboard: true
-        });
+    //     angular.element('#portfolio-modal').modal('show', {
+    //         keyboard: true
+    //     });
 
-        $('#portfolio-modal').on('shown.bs.modal', function () {
-            angular.element(".js-modal-image.active").removeClass("active");
-            angular.element(".js-modal-image").first().addClass('active');
-        }).on('hidden.bs.modal', function (e) {
-            angular.element(event.currentTarget).removeClass('active');
-        });
+    //     $('#portfolio-modal').on('shown.bs.modal', function () {
+    //         angular.element(".js-modal-image.active").removeClass("active");
+    //         angular.element(".js-modal-image").first().addClass('active');
+    //     }).on('hidden.bs.modal', function (e) {
+    //         angular.element(event.currentTarget).removeClass('active');
+    //     });
+    // }
+
+    function getPortfolioItemData(event){
+        return vm.portfolioItems[event.currentTarget.attributes['data-id'].value];
     }
 }
